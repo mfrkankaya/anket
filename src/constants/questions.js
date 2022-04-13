76579469ae90d19911acdfbdba880860657d49ca
Furkan Kaya <mfrkankaya@gmail.com>
2022-04-13 23:57:17 +0300

    update questions

--- src/constants/questions.js
+++ src/constants/questions.js
@@ -1,42 +1,138 @@
 export const questions = [
   {
     id: 1,
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."
+    text: 'Sınava girmeden de sınıf geçmenin ve başarılı olmanın bir yolu olmasını isterim.'
   },
   {
     id: 2,
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."
+    text: 'Çevremdekiler (ailem, arkadaşlarım) sınavları başaracağım konusunda bana güvenirler.'
   },
   {
     id: 3,
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."
+    text: 'Sınavlar sırasında, zihnimin sınavla ilgili olmayan konulara kaydığını hissederim.'
   },
   {
     id: 4,
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."
+    text: 'Sınavların mutlaka resmi, ciddi ve gerginlik yaratan durumlar olması gerekmez.'
   },
   {
     id: 5,
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."
+    text: 'Önemli sınavlardan önce veya sonra canım bir şey yemek istemez.'
   },
   {
     id: 6,
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."
+    text: 'Eğer sınavlar olmasaydı, dersleri daha iyi öğrenirdim.'
   },
   {
     id: 7,
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."
+    text: 'Başarı konusundaki endişelerim sınava hazırlığımı ve sınav başarımı etkiler.'
   },
   {
     id: 8,
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."
+    text: 'Önemli sınavlara gireceğim zaman uykularım kaçar.'
   },
   {
     id: 9,
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."
+    text: 'Başarısız olduğumda çevremdekilerin hakkımdaki düşünceleri beni rahatsız eder.'
   },
   {
     id: 10,
-    text: "Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy."
+    text: 'Başarısız olursam insanlar benim yeteneğimden şüpheye düşecekler.'
+  },
+  {
+    id: 11,
+    text: 'Sınavlardan önce bir türlü gevşeyemem.'
+  },
+  {
+    id: 12,
+    text: 'Önemli sınavlardan önce zihnim adeta durur.'
+  },
+  {
+    id: 13,
+    text: 'Sınavdan önce daima huzursuz, gergin ve sıkıntılı olurum.'
+  },
+  {
+    id: 14,
+    text: 'Sınavların insanın gelecekteki amaçlarına ulaşması konusunda ölçü olmasına hayret ederim.'
+  },
+  {
+    id: 15,
+    text: 'Sınavlar insanın gerçekten ne kadar bildiğini göstermez.'
+  },
+  {
+    id: 16,
+    text: 'Düşük not aldığımda, hiç kimseye notumu söylemem.'
+  },
+  {
+    id: 17,
+    text: 'Önemli Sınavlara çalışırken olumsuz düşüncelerle peşin bir yenilgi yaşarım.'
+  },
+  {
+    id: 18,
+    text: 'Sınav sonuçlarını almadan önce kendimi çok endişeli ve huzursuz hissederim.'
+  },
+  {
+    id: 19,
+    text: 'işe alınırken sınavlar veya test yapılmasını istemem.'
+  },
+  {
+    id: 20,
+    text: 'Sınavlarda başarılı olamazsam zannettiğim kadar akıllı olmadığımı düşünürüm.'
+  },
+  {
+    id: 21,
+    text: 'Sınavlarla ilgili endişelerim tam olarak hazırlanmamı engeller ve bu durum beni daha çok endişelendirir.'
+  },
+  {
+    id: 22,
+    text: 'Sınav sırasında bacağımı salladığımı, parmaklarımı sıraya vurduğumu fark ederim.'
+  },
+  {
+    id: 23,
+    text: 'Sınavlardan sonra gösterdiğim performanstan daha iyisini yapabileceğimi düşünürüm.'
+  },
+  {
+    id: 24,
+    text: 'Sınavlar sırasında duygularım dikkatimin dağılmasına neden olur.'
+  },
+  {
+    id: 25,
+    text: 'Başarısız olursam, kendimle ilgili görüşlerim değişir.'
+  },
+  {
+    id: 26,
+    text: 'Sınavlar sırasında bedenimin belirli yerlerindeki kaslar kasılır.'
+  },
+  {
+    id: 27,
+    text: 'Sınavlardan önce ne kendime tam olarak güvenebilirim ne de zihinsel olarak gevşeyebilirim.'
+  },
+  {
+    id: 28,
+    text: 'Başarısız olursam arkadaşlarımın gözünde değerimin düşeceğini bilirim.'
+  },
+  {
+    id: 29,
+    text: 'Önemli problemlerimden biri, bir sınava tam olarak hazırlanıp hazırlanmadığımı bilememektir.'
+  },
+  {
+    id: 30,
+    text: 'Gerçekten önemli sınavlara girerken bedensel olarak panik halinde olurum.'
+  },
+  {
+    id: 31,
+    text: 'Testi değerlendirenlerin, bazı öğrencilerin sınavda çok heyecanlandıklarını bilmelerini ve bu testi değerlendirirken bu durumu hesaba katmalarını isterim.'
+  },
+  {
+    id: 32,
+    text: 'Kendi notumu söylemeden önce arkadaşlarımın kaç aldığını bilmek isterim.'
+  },
+  {
+    id: 33,
+    text: 'Kırık not aldığım zaman tanıdığım bazı insanların benimle alay edeceğini bilirim ve bu beni rahatsız eder.'
+  },
+  {
+    id: 34,
+    text: 'Sınavlar sırasında gerçekten bildiklerimi unutacak kadar heyecanlanırım.'
   }
 ]
